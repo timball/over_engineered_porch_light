defad32b0d703251f67a8e301f6ecf32e48b2753
timball <timball+github@gmail.com>
2020-09-27 10:11:46 -0400

    add a load_conf utility

--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
 import logging
+import yaml
 
 def set_log_level(level):
     logger = logging.getLogger()
     logger.setLevel(level)
+
+def load_conf(config):
+    with open (config) as f: 
+        conf = yaml.load(f, yaml.FullLoader)
+    return conf
